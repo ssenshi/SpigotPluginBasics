f431a7d73de7fbbf4b55f1d4308bd0868643bd68
ssenshi <67323109+ssenshi@users.noreply.github.com>
2021-01-16 16:15:26 -0800

    Create FlyOnSurv.java

---
+++ FlyOnSurv.java
@@ -0,0 +1,35 @@
+
+import org.bukkit.Bukkit;
+import org.bukkit.GameMode;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerGameModeChangeEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+import net.md_5.bungee.api.ChatColor;
+
+
+public class FlyOnGMS extends JavaPlugin implements Listener{
+	
+	@Override
+	public void onEnable() {
+		
+		getServer().getPluginManager().registerEvents(this, this);
+		saveConfig();
+		
+		System.out.println("FlySpec plugin enabled [+]");
+	}
+	
+	@EventHandler
+	public void OnSurvivalMode(PlayerGameModeChangeEvent event) {
+		
+		System.out.println("Event is working correctly");
+		
+		Player player = event.getPlayer();
+		if (player.getGameMode() == GameMode.SPECTATOR) {
+			
+			Bukkit.getScheduler().runTaskLater(this, () -> player.setAllowFlight(true), 1L);
+			player.sendMessage(ChatColor.GRAY + "Your gamemode has succesfully changed to" + ChatColor.LIGHT_PURPLE + " spectator");
+		}
+	}
+}
