95da531825d307e5d246b9fa931c835bde93305b
ssenshi <67323109+ssenshi@users.noreply.github.com>
2021-02-11 07:25:23 -0800

    Create sounExample.java

---
+++ sounExample.java
@@ -0,0 +1,28 @@
+package dev.stan.mc;
+
+import org.bukkit.plugin.java.JavaPlugin; 
+
+import net.md_5.bungee.api.ChatColor; // importing chat color
+ 
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.player.AsyncPlayerChatEvent;
+import org.bukkit.Bukkit;
+import org.bukkit.Sound;
+import org.bukkit.event.Listener;
+
+public class ChatFilter extends JavaPlugin implements Listener{
+	
+	@Override
+	public void onEnable() {
+		System.out.println("Plugin has been enabled");
+		getServer().getPluginManager().registerEvents(this, this); 
+  }
+  
+  @EventHandler
+	public void onPlayerChat(AsyncPlayerChatEvent event) {
+		
+		Player player = event.getPlayer();	
+		player.getWorld().playSound(player.getLocation(), Sound.BLOCK_NOTE_BLOCK_BELL, 3.0F, 0.5F);
+    
+	}
