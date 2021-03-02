8dd233cd625f793cb5c395788f974e75526efeed
ssenshi <67323109+ssenshi@users.noreply.github.com>
2021-03-02 03:05:59 -0800

    Create CommandExecutor.java

---
+++ CommandExecutor.java
@@ -0,0 +1,16 @@
+package dev.stan.mc;
+
+import org.bukkit.event.Listener;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class CommandExecutor extends JavaPlugin implements Listener
+{
+	
+	@Override
+	public void onEnable() 
+	{
+		getLogger().info("Plugin enabled");
+		getCommand("{command name}").setExecutor(new {class name}());
+	}
+
+}
