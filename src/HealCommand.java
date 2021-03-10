7a243e178d386459bc19fa436bf8b15b128b1db6
dev-stan <67323109+dev-stan@users.noreply.github.com>
2021-03-10 19:31:23 +0100

    Update and rename SimpleHealCommand.java to src/HealCommand.java

--- src/HealCommand.java
+++ src/HealCommand.java
@@ -1,10 +1,6 @@
-package dev.stan.mc;
-
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.potion.PotionEffect;
-
 import net.md_5.bungee.api.ChatColor;
-
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
@@ -15,11 +11,6 @@ public class Main extends JavaPlugin {
 	@Override
 	public void onEnable() {
 		System.out.println("[blankPlugin] Enabled");
-	}
-	
-	@Override
-	public void onDisable() {
-		System.out.println("[blankPlugin] Disabled");
 	}
 	
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
@@ -33,14 +24,8 @@ public class Main extends JavaPlugin {
 				player.setHealth(20.0);
 			} else {
 				System.out.println("You cannot use this command through console!");
-			}
-			
-		}
-		
-		
+			}	
+		}		
 		return false;
-	}
-	
-	
+	}	
 }
-© 2021 GitHub, Inc.
